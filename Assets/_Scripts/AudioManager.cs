d27765320f1abb1d9f55f314cbd714546dd7de0a
GergoMuller <bejla94@gmail.com>
2017-10-20 10:08:28 +0200

    AudioManager

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	public Sound[] sounds;
+
+	void Start () {
+		foreach(Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+            sound.source.clip = sound.clip;
+        }
+	}
+	
+	public void playSound(string name)
+    {
+        Sound sound =Array.Find(sounds, s => s.name.Equals(name));
+        if(sound != null)
+        {
+            sound.source.Play();
+        }
+    }
+}
